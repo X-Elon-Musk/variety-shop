a2aa2654f7bfbdd8908c26dd4666b69624c2d518
792884274 <792884274@qq.com>
2018-03-21 15:20:07 +0800

    update

---
+++ test/document/main.js
@@ -0,0 +1,15 @@
+document.write('<div class="nav">'+
+		'<ul>'+
+			'<li><a href="http://2017igbc.cgigc.com.cn/" class="index-nav">首页</a></li>'+
+			'<li><a href="http://igbc.cgigc.com.cn/cjigbc2017intro/16451.html" target="_blank">大会简介</a></li>'+
+			'<li><a href="http://igbc.cgigc.com.cn/cjigbc2017news/" target="_blank">大会新闻</a></li>'+
+			'<li><a href="http://51y4gk5q-1-1-2-1-2.eventdove.com/" target="_blank">大会报名</a></li>'+
+			'<li><a href="http://igbc.cgigc.com.cn/cjigbc2017foreigntra/16454.html">海外厂商介绍</a></li>'+
+			'<li><a href="http://igbc.cgigc.com.cn/cjigbc2017schedules/16452.html" target="_blank">大会日程</a></li>'+
+			'<li><a href="" target="_blank">上届回顾</a></li>'+
+			'<li><a href="javascript:;" target="" class="connect_us go_footer">联系我们</a></li>'+
+		'</ul>'+
+    '</div>'+
+	'<script>'+
+		'console.log(22);'+
+	'</script>');
